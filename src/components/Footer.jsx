7aa9ce8baed485b179c2e0de41c4f190b8deef17
Aaron Durant <aaron@aarondurant.com>
2022-05-23 12:26:37 -0400

    Build area chart

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="text-center mt-12 mb-12">
+      Made by Aaron Durant in 2022
     </div>
   )
 }
